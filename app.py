314f9a9fea4299b2ce72f969d3ce51a566e7a55a
Kervin Prinvillee <kerv1234@live.fr>
2023-09-25 19:39:20 -0400

    Added instance id verification

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask
+import requests
+
 app = Flask(__name__)
+dns = "" #DNS of the EC2 instance
+
+if __name__ == '__main__':
+    # Set the port to 80
+    app.run(host='0.0.0.0', port=80)
+
 
 @app.route('/')
 def my_app():
-    return 'First Flask application!'
+    return get_instance_id()
+
+def get_instance_id(): #Get the AWS instance ID
+    url = "http://" + dns + "/latest/meta-data/instance-id"
+    r = requests.get(url)
+    return r.text
+
