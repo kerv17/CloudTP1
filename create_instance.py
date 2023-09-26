7fad19ba7d44b3a580bd54672315467a230f3bec
Kervin Prinvillee <kerv1234@live.fr>
2023-09-25 20:28:55 -0400

    CreateELB

--- create_instance.py
+++ create_instance.py
@@ -27,3 +27,43 @@ def create_instances(instance_type: str, count: int):
         instance.wait_until_running()
         print(f'Instance {instance.id} is running with public IP: {instance.public_ip_address}')
     return instances
+
+def createELB(name: str, subnets: list, security_groups: list):
+    elbv2 = boto3.client('elbv2',
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        region_name=region_name
+    )
+    load_balancer_name = name
+    listeners = [
+        {
+            'Protocol': 'HTTP',
+            'Port': 80,
+            'DefaultActions': [
+                {
+                    'Type': 'fixed-response',
+                    'FixedResponseConfig': {
+                        'ContentType': 'text/plain',
+                        'StatusCode': '200',
+                        'ContentType': 'text/plain',
+                        'ContentDescription': 'Hello from the load balancer!'
+                    }
+                }
+            ]
+        }
+    ]
+    response = elbv2.create_load_balancer(
+        Name=load_balancer_name,
+        Subnets=subnets,
+        SecurityGroups=security_groups,
+        Scheme='internet-facing',
+        Type='application',
+        IpAddressType='ipv4'
+    )
+
+    for listener in listeners:
+    elbv2.create_listener(
+        LoadBalancerArn=response['LoadBalancers'][0]['LoadBalancerArn'],
+        **listener
+    )
+    print(f"Load balancer {load_balancer_name} created successfully.")
