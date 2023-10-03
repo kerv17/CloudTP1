5b08a84c894a04c5fd6e861a8b84c72bcff3e38f
Kervin Prinville <prinvillek@mail.com>
2023-10-03 19:10:25 -0400

    runTerra.sh

---
+++ runTerra.sh
@@ -0,0 +1,40 @@
+################################
+# Setup AWS CLI
+################################
+
+export AWS_ACCESS_KEY_ID="$1"
+export AWS_SECRET_ACCESS_KEY="$2"
+export AWS_SESSION_TOKEN="$3"
+
+################################
+# Terraform Infra
+################################
+echo "Terraforming Infra..."
+
+git clone "https://github.com/kerv17/CloudTP1.git"
+
+cd CloudTP1
+cd Terraform
+terraform init
+terraform apply -auto-approve
+
+export url=$(terraform output --raw alb_dns_name)
+export load_balancer=$(terraform output --raw load_balancer_arn_suffix)
+export cluster1=$(terraform output --raw M4_group_arn_suffix)
+export cluster2=$(terraform output --raw T2_group_arn_suffix)
+cd ..
+cd ..
+
+echo "Terraforming completed!"
+
+################################
+# Requests
+################################
+
+docker pull kerv17/terra-requests:latest
+
+echo "Launching requests"
+docker run -e url="$url" kerv17/terra-requests:latest
+echo "Requests completed"
+
+$SHELL
